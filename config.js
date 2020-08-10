3d629f146da83a93ccd88cc16750527ee3567fe2
Rohit T P <tprohit9@gmail.com>
2020-08-10 14:27:44 +0530

    added eslint

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
+
 export const build_tree = [{
-    src_root: "src",
-    src_ts: "src/scripts",
-    dist_root: "dist",
-    dist_js: "dist/js",
-    sw: "service-worker",
-    precache: ["index.html"]
+	src_root: "src",
+	src_ts: "src/scripts",
+	dist_root: "dist",
+	dist_js: "dist/js",
+	sw: "service-worker",
+	precache: ["index.html"]
 }];
 
 export const format = "cjs";
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,23 @@
-import { precacheAndRoute } from 'workbox-precaching';
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst, NetworkFirst } from "workbox-strategies";
+import { ExpirationPlugin } from "workbox-expiration";
+import { setCacheNameDetails } from "workbox-core";
+
+setCacheNameDetails({ prefix: "__APP_NAME__", suffix: "__APP_VERSION__" });
 
 precacheAndRoute(self.__WB_MANIFEST || []);
+
+registerRoute(({ request }) => request.destination === "script", new NetworkFirst());
+
+registerRoute(({ request }) => request.destination === "image",
+	new CacheFirst({
+		cacheName: "images",
+		plugins: [
+			new ExpirationPlugin({
+				maxEntries: 60,
+				maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+			}),
+		],
+	}),
+);
